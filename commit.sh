55f87af9998fd41e5ebf8155e43e285d94b99c52
allenlsy <cafe@allenlsy.com>
2013-12-27 09:54:43 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# check sudo
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -9,14 +11,16 @@ DIR=$(pwd)
 # .vim
 echo "Committing .vim..."
 cd ~/.vim
-git add .
+git pull --rebase
+git aa
 git commit -a
 git push
 
 # .oh-my-zsh
 echo "Committing .oh-my-zsh..."
 cd ~/.oh-my-zsh
-git add .
+git pull --rebase
+git aa
 git commit -a
 git push
 
@@ -26,7 +30,9 @@ cd ~
 cp .gitconfig .gitignore .gvimrc .irbrc .tmux.conf .vimrc .zshrc $DIR
 
 cd $DIR
-git add --all
+git pull --rebase
+git submodule foreach git pull origin master
+git aa
 git commit -a
-git push -f
+git tf
 
